bae2a8740cf653646996003dc314aaa432a46e78
CarlosPrieto <carlosprieto66@icloud.com>
2023-02-01 14:22:14 -0800

    Responsive header with dropdown/slide in menu v-1

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("World");
+// DOM objects
+const menuToggle = document.querySelector(".menu-toggle");
+const menuItems = document.querySelector(".list-items");
+const menuIcon = document.querySelector("#menu_icon-img");
+
+
+
+menuToggle.addEventListener("click", ()=> {
+    menuItems.classList.toggle("hide");
+})
+// Listens for the window to be resized and the changes the menu style
+window.addEventListener("resize", ()=>{
+    if(window.innerWidth > 720) {
+        menuItems.classList.remove("dropdown", "hide");
+    } else {
+        menuItems.classList.add("dropdown", "hide");
+    }
+})
+// Changes the menu style from mobile to desktop depending on the screen size
+window.onload = ()=>{
+    if(window.innerWidth > 720) {
+        menuItems.classList.remove("dropdown", "hide");
+    } else {
+        menuItems.classList.add("dropdown", "hide");
+    }
+}
