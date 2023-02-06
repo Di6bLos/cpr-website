e12d98d387f76bbfed492003024e9e5916b2b73a
CarlosPrieto <carlosprieto66@icloud.com>
2023-02-06 15:32:12 -0800

    Set order or header items

--- script.js
+++ script.js
@@ -9,14 +9,6 @@ menuToggle.addEventListener("click", ()=> {
     menuItems.classList.toggle("hide");
     menuIcon.classList.toggle("open");
 })
-// Listens for the window to be resized and the changes the menu style
-window.addEventListener("resize", ()=>{
-    if(window.innerWidth > 720) {
-        menuItems.classList.remove("dropdown", "hide");
-    } else {
-        menuItems.classList.add("dropdown", "hide");
-    }
-})
 // Changes the menu style from mobile to desktop depending on the screen size
 window.onload = ()=>{
     if(window.innerWidth > 720) {
@@ -25,3 +17,14 @@ window.onload = ()=>{
         menuItems.classList.add("dropdown", "hide");
     }
 }
+// Listens for the window to be resized and the changes the menu style
+window.addEventListener("resize", ()=>{
+    if(window.innerWidth > 720) {
+        menuItems.classList.remove("dropdown", "hide");
+    } else {
+        menuItems.classList.add("dropdown", "hide");
+    }
+})
+
+
+
