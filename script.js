d745b08a592a387e3f7b67597089076ceec87994
CarlosPrieto <carlosprieto66@icloud.com>
2023-03-13 19:35:38 -0700

    bulk html markup, generic styling

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const menuItems = document.querySelector(".list-items");
 const menuIcon = document.querySelector(".menu-icon");
 
 
+// Array of Objects that will carry the Card information
+
+const servicesArr = [
+  {
+    title: "BLS / AED",
+    description: " Learn how to save lives in emergency situations with our BLS/AED certification course. This course covers basic life support techniques and AED usage, empowering you to confidently respond to cardiac and respiratory emergencies."
+  },
+  {
+    title: "CPR / AED",
+    description: "Equip yourself with the knowledge and skills to save lives with our CPR/AED certification course. Whether it's at work, home or in public, you'll learn the essential techniques for performing CPR and using an AED device, helping you become a valuable asset to your community."
+  },
+  {
+    title: "Standard First Aid",
+    description: "Be prepared to act in any emergency situation with our Standard First Aid certification course. Covering everything from wound care to allergic reactions, this course provides you with the tools and knowledge to respond confidently to any medical emergency."
+  }
+]
+
 // Opens and closes the menu, and changes the icon
 menuToggle.addEventListener("click", ()=> {
     menuItems.classList.toggle("hide");
